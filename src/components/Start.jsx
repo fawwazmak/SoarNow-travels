3e935402ad82a59c1547d43722b22e1c8999e247
fawwazmak <fawwazmakinde47@gmail.com>
2024-06-27 11:45:22 +0100

    working on the first page

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,9 +1,90 @@
-import React from 'react'
+import React from "react";
+import "../App.css";
 
 const Start = () => {
   return (
-    <div>Start</div>
-  )
-}
+    <>
+      <div className="text-light hero px-5">
+        <div className="">
+          <h1 className="text-center w-50 mx-auto">
+            Great Deals on Flight Tickets to All Destinations with Mytrip USA
+          </h1>
+        </div>
 
-export default Start
+        <form action="POST" className="bg-light text-dark rounded-3 row p-3">
+          <div className="col-lg-8 row">
+            <label className="col-lg-6 col-12 d-flex flex-column">
+              From
+              <input
+                style={{ height: "50px" }}
+                type="text"
+                placeholder="From"
+              />
+            </label>
+
+            <label className="col-lg-6 col-12 d-flex flex-column">
+              To
+              <input style={{ height: "50px" }} type="text" placeholder="To" />
+            </label>
+
+            <label className="col-lg-6 col-12 d-flex flex-column">
+              Departure
+              <input
+                style={{ height: "50px" }}
+                type="date"
+                placeholder="Departure"
+              />
+            </label>
+
+            <label className="col-lg-6 col-12 d-flex flex-column">
+              Return
+              <input
+                style={{ height: "50px" }}
+                type="date"
+                placeholder="Return"
+              />
+            </label>
+
+            <label className="col-lg-6 col-12 d-flex flex-column">
+              Passengers
+              <input
+                style={{ height: "50px" }}
+                type="text"
+                placeholder="Passengers"
+              />
+            </label>
+            <label className="col-lg-6 col-12 d-flex flex-column">
+              Economy
+              <input
+                style={{ height: "50px" }}
+                type="text"
+                placeholder="Economy"
+              />
+            </label>
+          </div>
+
+          <div className="col-lg-4 d-flex flex-column-reverse flex-lg-column justify-content-between">
+            <input
+              style={{ height: "50px" }}
+              type="button"
+              value="Submit"
+              className="bg-success rounded-3 border-none"
+            />
+
+            <label className="d-flex gap-2 align-items-center">
+              <input
+                type="checkbox"
+                style={{ height: "30px", width: "30px" }}
+                name="Non-stop"
+                id="non-stop"
+              />
+              Nonstop flights only
+            </label>
+          </div>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default Start;
--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,27 +1,49 @@
 import React from "react";
-import { Link } from "react-router-dom"
-import {Container, Navbar, Nav, NavDropdown,} from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
 
 const NavScrollExample = () => {
   return (
     <>
-      <Navbar expand="lg" className="bg-body-tertiary">
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary position-sticky top-0 w-100"
+      >
         <Container fluid>
-          <Navbar.Brand><Link to="/" className="text-decoration-none text-dark">LOGO</Link></Navbar.Brand>
+          <Navbar.Brand>
+            <Link to="/" className="text-decoration-none text-dark">
+              LOGO
+            </Link>
+          </Navbar.Brand>
 
           <Navbar.Toggle aria-controls="navbarScroll" />
           <Navbar.Collapse id="navbarScroll">
-            <Nav className="d-flex gap-3 me-auto ms-lg-5" style={{ maxHeight: "100px" }} navbarScroll>
-              <Link to="/Flight" className="text-decoration-none text-dark">Flight</Link>
-              <Link to="/Visas" className="text-decoration-none text-dark">Visas</Link>
-              <Link to="/Holiday" className="text-decoration-none text-dark">Holiday</Link>
-              <Link to="/Education-easy-pay" className="text-decoration-none text-dark">Education Easy-Pay</Link>
+            <Nav
+              className="d-flex gap-3 me-auto ms-lg-5"
+              style={{ maxHeight: "100px" }}
+              navbarScroll
+            >
+              <Link to="/Flight" className="text-decoration-none text-dark">
+                Flight
+              </Link>
+              <Link to="/Visas" className="text-decoration-none text-dark">
+                Visas
+              </Link>
+              <Link to="/Holiday" className="text-decoration-none text-dark">
+                Holiday
+              </Link>
+              <Link
+                to="/Education-easy-pay"
+                className="text-decoration-none text-dark"
+              >
+                Education Easy-Pay
+              </Link>
             </Nav>
-              <NavDropdown title="Support" id="navbarScrollingDropdown">
-                <NavDropdown.Item>Phone: 08074495421</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item>Email: SoarNow@gmail.com</NavDropdown.Item>
-              </NavDropdown>
+            <NavDropdown title="Support" id="navbarScrollingDropdown">
+              <NavDropdown.Item>Phone: 08074495421</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item>Email: SoarNow@gmail.com</NavDropdown.Item>
+            </NavDropdown>
           </Navbar.Collapse>
         </Container>
       </Navbar>
