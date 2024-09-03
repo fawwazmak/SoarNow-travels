1f080c1ae0574091baf3ef7c047a3a1278bbaadd
unknown <you@example.com>
2024-09-03 20:08:21 +0100

    hmmm

--- src/components/Start.jsx
+++ src/components/Start.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from "react";
 import "../App.css";
+import { LuArrowUpRightFromCircle } from "react-icons/lu";
+import { LuArrowDownToDot } from "react-icons/lu";
 import Button from "./Button";
 
 const Start = () => {
@@ -18,95 +20,105 @@ const Start = () => {
           </h1>
         </div>
 
-        <form action="POST" className="bg-light text-dark rounded-3 row p-3">
-          {/* Travel type  */}
-          <div className="d-flex gap-3 flex-sm-row flex-column mb-3">
-            <div className="d-flex gap-2">
-              <input className="d-block" checked={travelType === 'return'} onChange={travelTypeChange} value="return" type="radio" name="travel-type" id="return" />
-              <label className="d-block" htmlFor="return">Return</label>
-            </div>
+       <form action="" method="post" className="bg-white p-4 rounded">
 
-            <div className="d-flex gap-2">
-              <input className="d-block" checked={travelType === 'one way'} onChange={travelTypeChange} value="one way" type="radio" name="travel-type" id="one-way" />
-              <label className="d-block" htmlFor="one-way">One-Way</label>
-            </div>
+        {/* Travel type  */}
+        <div className="d-flex gap-3 flex-sm-row flex-column">
+          <div className="d-flex gap-2">
+            <input className="d-block" checked={travelType === 'return'} onChange={travelTypeChange} value="return" type="radio" name="travel-type" id="return" />
+            <label className="d-block text-black" htmlFor="return">Return</label>
+          </div>
+
+          <div className="d-flex gap-2">
+            <input className="d-block" checked={travelType === 'oneWay'} onChange={travelTypeChange} value="oneWay" type="radio" name="travel-type" id="one-way" />
+            <label className="d-block text-black" htmlFor="one-way">One-Way</label>
+          </div>
             
-            <div className="d-flex gap-2">
-              <input className="d-block" checked={travelType === 'multi city'} onChange={travelTypeChange} value="multi city" type="radio" name="travel-type" id="multi-city" />
-              <label className="d-block" htmlFor="multi-city">Multi-City</label>
-            </div>
+          <div className="d-flex gap-2">
+            <input className="d-block" checked={travelType === 'multiCity'} onChange={travelTypeChange} value="multiCity" type="radio" name="travel-type" id="multi-city" />
+            <label className="d-block text-black" htmlFor="multi-city">Multi-City</label>
           </div>
+        </div>
 
+        {(travelType !== 'multiCity') && 
+          <>
+              <div>
+                <div className="text-dark">
+                  <div className="d-flex flex-lg-row flex-column gap-lg-5 gap-2">
+                    <div className="w-100">
+                      <label htmlFor="startingLocation">From</label>
+                      <div className="border border-1 border-dark px-2 py-2 d-flex align-items-center">
+                        <LuArrowUpRightFromCircle />
+                        <input className="border-0 w-100" type="text" name="startingLocation" id="startingLocation" />
+                      </div>
+                    </div>
+                    <div className="w-100">
+                      <label htmlFor="stoppingLocation">To</label>
+                      <div className="border border-1 border-dark px-2 py-2 d-flex align-items-center">
+                        <LuArrowDownToDot />
+                        <input className="border-0 w-100" type="text" name="stoppingLocation" id="stoppingLocation" />
+                      </div>
+                    </div>
+                  </div>
 
+                  <div className="d-flex flex-lg-row flex-column gap-lg-5 gap-2">
+                    {/* Deaparture and return  */}
+                    <div className="d-flex w-100">
+                      {/* DepartTure date  */}
+                      <div className="w-100">
+                        <label htmlFor="departureDate">Departure</label>
 
-          <div className="col-lg-8 row">
-            <label className="col-lg-6 col-12 d-flex flex-column">
-              From
-              <input
-                style={{ height: "50px" }}
-                type="text"
-                placeholder="From"
-              />
-            </label>
+                        <div className="border-1 border-dark border py-2 px-2">
+                          <input className="border-0 w-100" type="date" name="departureDate" id="departureDate" />
+                        </div>
+                      </div>
 
-            <label className="col-lg-6 col-12 d-flex flex-column">
-              To
-              <input style={{ height: "50px" }} type="text" placeholder="To" />
-            </label>
+                      {/* Return date  */}
+                      <div className="w-100">
+                        <label htmlFor="returnDate">Return</label>
 
+                        <div className="border-1 border-dark border py-2 px-2">
+                          <input className="border-0 w-100" type="date" name="returnDate" id="returnDate" />
+                        </div>
+                      </div>
+                    </div>
 
-            {/* This will display for multi-city choice only  */}
-            
-            <label className="col-lg-6 col-12 d-flex flex-column">
-              Departure
-              <input
-                style={{ height: "50px" }}
-                type="date"
-                placeholder="Departure"
-              />
-            </label>
+                    {/* Passengers and class  */}
+                    <div className="d-flex w-100">
 
-            <label className="col-lg-6 col-12 d-flex flex-column">
-              Return
-              <input
-                style={{ height: "50px" }}
-                type="date"
-                placeholder="Return"
-              />
-            </label>
+                      {/* Number of passengers  */}
+                      <div className="w-100">
+                        <label htmlFor="passengers">Passengers</label>
 
-            <label className="col-lg-6 col-12 d-flex flex-column">
-              Passengers
-              <input
-                style={{ height: "50px" }}
-                type="text"
-                placeholder="Passengers"
-              />
-            </label>
-            <label className="col-lg-6 col-12 d-flex flex-column">
-              Economy
-              <input
-                style={{ height: "50px" }}
-                type="text"
-                placeholder="Economy"
-              />
-            </label>
-          </div>
+                        <div className="border-1 border-dark border p-2">
+                          <input className="border-0 w-100" type="text" name="passengers" id="passengers" />
+                        </div>
+                      </div>
 
-          <div className="col-lg-4 d-flex flex-column-reverse flex-lg-column justify-content-between">
-            <Button btnText="Submit" classB={"mt-4 mt-lg-0"} />
+                      {/* Travel class  */}
+                      <div className="w-100">
+                        <label htmlFor="travelClass">Class</label>
 
-            <label className="d-flex gap-2 align-items-center">
-              <input
-                type="checkbox"
-                style={{ height: "30px", width: "30px" }}
-                name="Non-stop"
-                id="non-stop"
-              />
-              Nonstop flights only
-            </label>
-          </div>
-        </form>
+                        <div className="border-1 border-dark border p-2">
+                          <select name="travelClass" id="travelClass" className="border-0 w-100">
+                            <option value="economy">Economy</option>
+                            <option value="premium">Premium</option>
+                            <option value="business">Business</option>
+                            <option value="first">First</option>
+                          </select>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div></div>
+              </div>
+          </>
+          
+        }
+
+        <small className="text-success ">**( Specify Passenger(s): adult, children, infant )</small>
+       </form>
       </div>
     </>
   );
